280b86dcd6cc547011ed5229b1749d7f9492858c
antonproshkovsky <antonproshkovsky@gmail.com>
2019-12-06 16:41:03 +0200

    scroll modules

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,13 @@
-alert('test');alert('test');
+import {Scrolling} from "./modules/Scrolling";
+import $ from "jquery";
 
+const sc = new Scrolling($('.scrolling-content'), {
+  theme:"dark",
+  scrollInertia: 700,
+  scrollEasing:"easeInOut",
+  snapAmount: 70
+});
+
+console.log(sc);
+
+sc.init();
--- assets/js/modules/Scrolling.js
+++ assets/js/modules/Scrolling.js
@@ -2,9 +2,56 @@ import $ from 'jquery'
 import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min';
 
 
-export class Scrolling{
-  constructor(){
+export class Scrolling {
+  constructor($container, options) {
+    this.$container = $container;
+    this.options = options;
 
+    this.$parallaxElements = this.$container.find('[data-scroll-speed]');
+    this.$scrollToElements = this.$container.find('[data-scroll-to]');
+  }
+
+  initParallax(self) {
+    const {$parallaxElements} = self;
+
+    let scrollTop = this.mcs.top;
+
+    $parallaxElements.each((index, element) => {
+      if($(element).is(':mcsInSight') || $(element).css('transform') == 'none') {
+        var scrollSpeed = parseInt($(element).data('scroll-speed')),
+          val = -scrollTop * (scrollSpeed / 5);
+        $(element).css('transform', 'translateY(' + val + 'px)');
+      }
+    });
+  }
+
+  initScrollTo(){
+    const self = this;
+
+    this.$scrollToElements.click(function (e) {
+      e.preventDefault();
+
+      const id = $(this).attr('data-scroll-to');
+      self.$container.mCustomScrollbar('scrollTo',$(id));
+    })
+  }
+
+  init() {
+    const self = this;
+    const {options} = self;
+
+    $(window).on('load',() => {
+      this.$container.mCustomScrollbar({
+        ...options,
+        callbacks: {
+          whileScrolling() {
+            self.initParallax.apply(this, [self]);
+          }
+        }
+      });
+
+      self.initScrollTo();
+    })
   }
 }
 
